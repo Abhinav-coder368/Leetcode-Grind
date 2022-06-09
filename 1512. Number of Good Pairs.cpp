616b9a7abb3b0ad427da090bd5cd7fde2a371fe3
Abhinav Gangwar <76556517+Abhinav-coder368@users.noreply.github.com>
2022-06-10 00:45:05 +0530

    Update 1512. Number of Good Pairs.cpp

--- 1512. Number of Good Pairs.cpp
+++ 1512. Number of Good Pairs.cpp
@@ -14,3 +14,19 @@ public:
 };
 
 
+// using map
+
+
+// int numIdenticalPairs(vector<int>& nums) {
+//         int n=nums.size();
+//         int total=0;
+//         map<int ,int> mp;
+//         for(int i=0;i<n;i++){
+//             total+=mp[nums[i]]; //and before incrementing the count i'm finding how many numbers are there before this  
+//             mp[nums[i]]++;   // in the map mp i'm storing the nums[i] and increment it's count. and before incrementing the 
+//                                  count i'm finding how many numbers are there before this
+//         }
+//     return total;
+//     }
+
+
