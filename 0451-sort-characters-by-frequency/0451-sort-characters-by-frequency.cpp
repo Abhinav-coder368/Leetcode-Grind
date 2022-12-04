702eed6aeecc0d69232678bc1873c16f2165a07a
Abhinav Gangwar <76556517+Abhinav-coder368@users.noreply.github.com>
2022-12-04 16:41:33 +0530

    Time: 26 ms (54.89%), Space: 8.5 MB (40.14%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
+    // static bool cmp(pair<char,int>&a,pair<char,int>&b){
+    //     return a.second>b.second;
+    // }
     string frequencySort(string s) {
- map<char,int>mp;
- string ps;
- for(int i=0;i<s.size();i++){
-    mp[s[i]]++;
- }
-vector<pair<int,char>>p;
-for(auto it:mp){
-    p.push_back({it.second,it.first});
-}
-sort(p.rbegin(),p.rend());
-for(int i=0;i<p.size();i++){
-    int x=p[i].first;
-    while(x--){
-        ps+=p[i].second;
-    }
-}
-return ps;
+       
+        string ans = "";
+        map<char,int> mp;
+        for(char c : s)
+            mp[c]++;
+        
+        vector<pair<int,char>>res;
+        for(auto it:mp) res.push_back({it.second,it.first});
+        sort(res.rbegin(),res.rend());
+        for(auto it : res){
+                int x = it.first;
+                while(x--)
+                  ans+=it.second;   
+            }
+        
+        
+        return ans;
     }
 };
