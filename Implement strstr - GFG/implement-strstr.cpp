6a90ff5a2483ce639fe60bcf12674d3745ff3f10
Abhinav Gangwar <76556517+Abhinav-coder368@users.noreply.github.com>
2022-12-09 16:16:32 +0530

    Added solution - LeetHub

---
+++ Implement strstr - GFG/implement-strstr.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+int strstr(string ,string);
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string a;
+        string b;
+        
+        cin>>a;
+        cin>>b;
+        
+        cout<<strstr(a,b)<<endl;
+    }
+}
+
+// } Driver Code Ends
+
+
+//Function to locate the occurrence of the string x in the string s.
+int strstr(string s, string x)
+{
+    int pos =0,index;
+     if((index = s.find(x,pos)) != string::npos){
+       pos = index+1;
+     }
+     if(pos==0) return -1;
+     return pos-1;
+       
+}
