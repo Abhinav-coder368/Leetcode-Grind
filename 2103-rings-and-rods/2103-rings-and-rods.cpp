00d24f80eb7a7e42433849b797ffe5ca08027dbf
Abhinav Gangwar <76556517+Abhinav-coder368@users.noreply.github.com>
2023-01-30 15:45:38 +0530

    Time: 2 ms (51.58%), Space: 6.6 MB (26.01%) - LeetHub

--- 2103-rings-and-rods/2103-rings-and-rods.cpp
+++ 2103-rings-and-rods/2103-rings-and-rods.cpp
@@ -4,18 +4,23 @@ public:
         
         // for(int i=0;i<rings.size();i++)
         //     cout<<rings[i];
-        map<char,vector<char>> mp;
+        map<char,vector<char>> mp;                // creating map of vector 
         for(int i=0;i<rings.size()-1;i++){
-            mp[rings[i+1]].push_back(rings[i]);
-            i++;
+            mp[rings[i+1]].push_back(rings[i]);  // ways to insert data into map of vector
+            i++;                                 // i++ because we're taking alternate value 
+                                                // excluding char
         }
-int cnt=0;
-        for(auto it  : mp){
-        set<char>st;
-            for(auto i:it.second){
-                st.insert(i);
+        
+        int cnt=0;
+        
+        for(auto it  : mp){             // 0 --- BGR
+                                        // 6 --- BRR
+                                        // 9 --- G
+        set<char>st;                         // redecalaring set to 0;
+            for(auto i : it.second){    // for every it.second value it is inserting into set
+                st.insert(i);           // (B) GR    :::>  inserting B into set then G then R;
             }
-            if(st.size()==3)  cnt++;
+            if(st.size()==3)  cnt++;    // set.size()==3 cnt++
         }
        
  
