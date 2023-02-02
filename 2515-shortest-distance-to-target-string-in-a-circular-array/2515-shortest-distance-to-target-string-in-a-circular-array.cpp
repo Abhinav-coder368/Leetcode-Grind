4934746d5ec9a88c3c39e5f286fdf4efd513eaa6
Abhinav Gangwar <76556517+Abhinav-coder368@users.noreply.github.com>
2023-02-02 22:59:30 +0530

    Time: 29 ms (88.72%), Space: 14.6 MB (62.52%) - LeetHub

---
+++ 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int closetTarget(vector<string>& words, string target, int startIndex) {
+        if (words[startIndex] == target)
+        return 0;
+        bool isTargetPresent = false;
+        for(int i=0;i<words.size();i++){
+            if(words[i]==target){
+                isTargetPresent=true;
+                break;
+            }
+        }
+        if(isTargetPresent==false)return -1;
+
+        int n = words.size();
+        int forwardCount=0;
+        for(int i=startIndex;i<2*n;i++){
+            forwardCount++;
+            if(words[(i + 1) % n] == target){
+                // forwardCount = i;
+                break;
+             }
+                                    
+        }
+
+
+    int backwardCount=0;
+        for(int j=startIndex;j>-n;j--){
+            backwardCount++;
+            if(words[(j - 1 + n) % n] == target){
+                // backwardCount = j;
+                break;
+            }
+            // if(j==0) 
+            //     j = n-1;
+                     
+        }
+
+//         int a = abs(backwardCount - startIndex + 1);
+//         int b = abs(forwardCount - startIndex + 1);
+        return min(backwardCount,forwardCount);
+    }
+};
