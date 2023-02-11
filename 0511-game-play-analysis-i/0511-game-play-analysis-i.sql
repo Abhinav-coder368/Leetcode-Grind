cd87e81e30260c6f627e7e977fcb0183854d0320
Abhinav Gangwar <76556517+Abhinav-coder368@users.noreply.github.com>
2023-02-11 16:21:54 +0530

    Time: 972 ms (35.88%), Space: 0B (100.00%) - LeetHub

---
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT DISTINCT
+  A.player_id,
+  FIRST_VALUE(A.event_date) OVER (
+    PARTITION BY
+      A.player_id
+    ORDER BY
+      A.event_date
+  ) AS first_login
+FROM
+  Activity A;
