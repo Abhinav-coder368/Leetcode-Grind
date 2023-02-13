e92dfc93de6873bb6ff09d391bbbe458724eaf23
Abhinav Gangwar <76556517+Abhinav-coder368@users.noreply.github.com>
2023-02-13 20:55:27 +0530

    Time: 3 ms (66.30%), Space: 6 MB (63.61%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
@@ -4,11 +4,7 @@ public:
         int cnt = (high-low)+1, ans;
         if(low%2==1 and high%2==1)
         ans = (cnt/2)+1;
-        if(low%2==0 and high%2==0)
-        ans = cnt/2;
-        if(low%2==1 and high%2==0)     //10 - 3 = 7+1 = 8
-        ans = cnt/2;
-        if(low%2==0 and high%2==1)     //7 - 2 = 5+1 = 6
+        if(low%2==0 and high%2==0  ||  low%2==0 and high%2==1 || low%2==1 and high%2==0)
         ans = cnt/2;
         
         return ans;
