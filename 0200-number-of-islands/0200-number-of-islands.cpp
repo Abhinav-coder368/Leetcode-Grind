682423f8822463de29350dbc8a4595c13b4d3b38
Abhinav Gangwar <76556517+Abhinav-coder368@users.noreply.github.com>
2023-04-10 10:25:14 +0530

    Time: 51 ms (18.99%), Space: 19.1 MB (17.09%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -0,0 +1,53 @@
+class Solution {
+    private:
+    void bfs(int row, int col, vector<vector<int>> &vis, vector<vector<char>>&grid,
+            int delrow[],int delcol[]) {
+      // mark it visited
+      vis[row][col] = 1; 
+      queue<pair<int,int>> q;
+      // push the node in queue
+      q.push({row, col}); 
+      int n = grid.size(); 
+      int m = grid[0].size(); 
+      
+      // until the queue becomes empty
+      while(!q.empty()) {
+          int row = q.front().first; 
+          int col = q.front().second; 
+          q.pop(); 
+          
+          // traverse in the neighbours and mark them if its a land 
+          for(int i=0;i<4;i++){
+                  int nrow = row + delrow[i]; 
+                  int ncol = col + delcol[i]; 
+                  // neighbour row and column is valid, and is an unvisited land
+                  if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m 
+                  && grid[nrow][ncol] == '1' && !vis[nrow][ncol]) {
+                      vis[nrow][ncol] = 1; 
+                      q.push({nrow, ncol}); 
+                  }
+              
+          }
+      }
+  }
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int n = grid.size(); 
+        int m = grid[0].size(); 
+        // create visited array and initialise to 0
+        vector<vector<int>> vis(n, vector<int>(m, 0)); 
+        int cnt = 0; 
+        int delrow[] = {-1, 0, 1, 0};
+        int delcol[] = {0, 1, 0, -1};
+        for(int row = 0; row < n ; row++) {
+            for(int col = 0; col < m ;col++) {
+                // if not visited and is a land
+                if(!vis[row][col] && grid[row][col] == '1') {
+                    cnt++; 
+                    bfs(row, col, vis, grid,delrow,delcol); 
+                }
+            }
+        }
+        return cnt; 
+    }
+};
