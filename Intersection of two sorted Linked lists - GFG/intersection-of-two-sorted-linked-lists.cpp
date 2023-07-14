57da978dd9c8c569c9c1f74f9eb3fefeabc51ad4
Abhinav Gangwar <76556517+Abhinav-coder368@users.noreply.github.com>
2023-07-14 13:47:50 +0530

    Added solution - LeetHub

---
+++ Intersection of two sorted Linked lists - GFG/intersection-of-two-sorted-linked-lists.cpp
@@ -0,0 +1,108 @@
+//{ Driver Code Starts
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+Node* inputList(int size)
+{
+    Node *head, *tail;
+    int val;
+    
+    cin>>val;
+    head = tail = new Node(val);
+    
+    while(--size)
+    {
+        cin>>val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+    
+    return head;
+}
+
+void printList(Node* n)
+{
+    while(n)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+}
+
+Node* findIntersection(Node* head1, Node* head2);
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,m;
+	    cin>> n >> m;
+	    
+	    Node* head1 = inputList(n);
+	    Node* head2 = inputList(m);
+	    
+	    Node* result = findIntersection(head1, head2);
+	    
+	    printList(result);
+	    cout<< endl;
+	}
+	return 0;
+}
+
+// } Driver Code Ends
+
+
+/* The structure of the Linked list Node is as follows:
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+*/
+
+Node* findIntersection(Node* head1, Node* head2)
+{
+    // Your Code Here
+    Node* head = new Node(-1);
+        Node* temp = head;
+        
+        map<int,int> mp;
+        while(head2 != NULL){
+            mp[head2->data]++;
+            head2 = head2->next;
+        }
+        while(head1 != NULL){
+            if(mp.find(head1->data) != mp.end()){
+                Node* ans = new Node(head1->data);
+                temp->next = ans;
+                temp = ans;
+                mp[head1->data]--;
+                if(mp[temp->data]==0)
+                   mp.erase(temp->data);
+            }
+            head1 = head1 -> next;
+        }
+        return head->next;
+}
