8e7b12283bd87c3b220b9ac19ca36a9c8e4b336f
Abhinav Gangwar <76556517+Abhinav-coder368@users.noreply.github.com>
2023-08-13 18:38:03 +0530

    Added solution - LeetHub

--- Grid Path 2 - GFG/grid-path-2.cpp
+++ Grid Path 2 - GFG/grid-path-2.cpp
@@ -6,23 +6,25 @@ using namespace std;
 class Solution {
   public:
   #define mod (int)(1e9+7)
-    int f(int i,int j,int n,int m,vector<vector<int>>& grid,vector<vector<int>>& dp)
+    int f(int i,int j,vector<vector<int>>& grid,vector<vector<int>>& dp)
     {
-        if(i>=n || j>=m || grid[i][j]==1) return 0;
+        if(i>=0 and j>=0 and grid[i][j]==1) return 0;  // for dead cell
         
-        if(i==n-1 && j==m-1) return 1;
+        if(i==0 && j==0) return 1;
+        
+        if(i<0 or j<0) return 0;
         
         if(dp[i][j]!=-1) return dp[i][j];
         
-        int down=f(i+1,j,n,m,grid,dp);
+        int down=f(i-1,j,grid,dp);
         
-        int right=f(i,j+1,n,m,grid,dp);
+        int right=f(i,j-1,grid,dp);
         
         return dp[i][j]=(down+right)%mod;
     }
     int totalWays(int n, int m, vector<vector<int>> &grid) {
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        return f(0,0,n,m,grid,dp);
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        return f(n-1,m-1,grid,dp);
     }
     
 };
