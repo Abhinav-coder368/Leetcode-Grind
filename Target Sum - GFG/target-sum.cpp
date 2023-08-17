df6c356c1acf236eee88d4a8ff6d597bde6920b4
Abhinav Gangwar <76556517+Abhinav-coder368@users.noreply.github.com>
2023-08-17 18:51:54 +0530

    Added solution - LeetHub

---
+++ Target Sum - GFG/target-sum.cpp
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//Back-end complete function Template for C++
+
+class Solution {
+  public:
+  int findWaysUtil(int ind, int target, vector<int>& arr, vector<vector<int>> &dp){
+
+    if(ind == 0){
+            if(target==0 && arr[0]==0)
+                return 2;
+            if(target==0 || target == arr[0])
+                return 1;
+            return 0;
+    }
+    
+    if(dp[ind][target]!=-1)
+        return dp[ind][target];
+        
+    int notTaken = findWaysUtil(ind-1,target,arr,dp);
+    
+    int taken = 0;
+    if(arr[ind]<=target)
+        taken = findWaysUtil(ind-1,target-arr[ind],arr,dp);
+        
+    return dp[ind][target]= notTaken + taken;
+}
+
+int findWays(vector<int> &num, int k){
+    int n = num.size();
+    vector<vector<int>> dp(n,vector<int>(k+1,-1));
+    return findWaysUtil(n-1,k,num,dp);
+}
+    int findTargetSumWays(vector<int>&A ,int target) {
+        //Your code here
+        int n = A.size();
+        int totSum = 0;
+        for(int i=0; i<n;i++){
+            totSum += A[i];
+        }
+    
+    //Checking for edge cases
+    if(totSum-target <0 || (totSum-target)%2 ) return 0;
+    
+    return findWays(A,(totSum-target)/2);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        
+        cin>>N;
+        vector<int>arr(N);
+        
+        for(int i=0 ; i<N ; i++){
+            cin>>arr[i];
+        }
+        int target;
+        cin >> target;
+
+        Solution ob;
+        cout<<ob.findTargetSumWays(arr,target);
+        cout<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
