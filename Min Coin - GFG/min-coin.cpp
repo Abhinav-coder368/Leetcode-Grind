db177874f9f91b8e4e13084309b6def0d07fc6ef
Abhinav Gangwar <76556517+Abhinav-coder368@users.noreply.github.com>
2023-08-17 15:16:31 +0530

    Added solution - LeetHub

---
+++ Min Coin - GFG/min-coin.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	public:
+	int minimumElementsUtil(vector<int>& arr, int ind, int T, vector<vector<int>>& dp){
+
+    if(ind == 0){
+        if(T%arr[0] == 0) return T/arr[0];
+        else return 1e9;
+    }
+    
+    if(dp[ind][T]!=-1)
+        return dp[ind][T];
+        
+    int notTaken = 0 + minimumElementsUtil(arr,ind-1,T,dp);
+    
+    int taken = 1e9;
+    if(arr[ind] <= T)
+        taken = 1 + minimumElementsUtil(arr,ind,T-arr[ind],dp);
+        
+    return dp[ind][T] = min(notTaken,taken);
+}
+	int MinCoin(vector<int>arr, int T)
+	{
+	   int n= arr.size();
+    
+    vector<vector<int>> dp(n,vector<int>(T+1,-1));
+    int ans =  minimumElementsUtil(arr, n-1, T, dp);
+    if(ans >= 1e9) return -1;
+    return ans;
+	}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, amount;
+		cin >> n >> amount;
+		vector<int>nums(n);
+		for(int i = 0; i < n; i++)
+			cin >> nums[i];
+		Solution ob;
+		int ans = ob.MinCoin(nums, amount);
+		cout << ans <<"\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
