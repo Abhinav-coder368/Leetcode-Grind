1e25a88e574ad5b281c252bb9ed90017bdc99bd4
Abhinav Gangwar <76556517+Abhinav-coder368@users.noreply.github.com>
2023-08-31 10:02:23 +0530

    Added solution - LeetHub

---
+++ Pattern 3 - GFG/pattern-3.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printTriangle(int n) {
+        // code here
+        int cnt = 1;
+        for(int i=1;i<=n;i++){
+            int temp = cnt;
+            for(int i=1;i<=cnt;i++){
+                cout<<i<<" ";
+            }
+            cout<<endl;
+            cnt++;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
